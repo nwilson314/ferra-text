e4255afa195e0237b9c71027fa517265f136fd83
nwilson314 <nsw.wilson@gmail.com>
2024-11-13 11:31:42 -0500

    welcome message and better traits

--- src/editor.rs
+++ src/editor.rs
@@ -2,6 +2,9 @@ use crate::{Position, Size, Terminal};
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use std::io::Error;
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
 }
@@ -57,13 +60,34 @@ impl Editor {
         Ok(())
     }
 
+    fn draw_welcome_message() -> Result<(), Error> {
+        let mut welcome_message = format!("{} v{}", NAME, VERSION);
+        let width = Terminal::size()?.width;
+        let len = welcome_message.len();
+        let padding = (width.saturating_sub(len)) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{spaces}{welcome_message}");
+        welcome_message.truncate(width);
+        Terminal::print(welcome_message)?;
+        Ok(())
+    }
+
+    fn draw_empty_row() -> Result<(), Error> {
+        Terminal::print("~")?;
+        Ok(())
+    }
+
     fn draw_rows() -> Result<(), Error> {
         let Size { height, .. } = Terminal::size()?;
 
         for current_row in 0..height {
             Terminal::clear_line()?;
-            Terminal::print("~")?;
-            if current_row + 1 < height {
+            if current_row == height / 3 {
+                Self::draw_welcome_message()?;
+            } else {
+                Self::draw_empty_row()?;
+            }
+            if current_row.saturating_add(1) < height {
                 Terminal::print("\r\n")?;
             }
         }
